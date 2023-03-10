46739f3a133ae40d41c5fd5c462f12e0334feec1
Henri Nurmi <henri@nurmi.me>
2023-03-11 00:42:07 +0200

    Add tmux session launcher

---
+++ shell/aliases.sh
@@ -0,0 +1,11 @@
+# NeoVim related
+alias v='vim'
+if command -v nvim &> /dev/null; then
+    alias vim='nvim'
+fi
+
+# Git related
+alias wa='git worktree-add'
+
+# Tmux related
+alias t='tmux-new-session'
